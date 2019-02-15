31dcc55709e03e1f8733881fae3cb0a7e822e16c
ralf.lang@ralf-lang.de <lang@b1-systems.de>
2019-02-15 13:14:45 +0000

    partial support for horde-application type specific behaviour

--- src/HordeInstaller.php
+++ src/HordeInstaller.php
@@ -18,7 +18,7 @@ class HordeInstaller extends LibraryInstaller
                 return $package->getPrettyName();
             break;
             case 'horde-library':
-                return 'libs/' . $package->getPrettyName();
+                return parent::getInstallPath($package);
             break;
             case 'horde-theme':
                 return 'themes/' . $package->getPrettyName();
@@ -30,11 +30,30 @@ class HordeInstaller extends LibraryInstaller
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
         parent::install($repo, $package);
-        // horde-application needs a config/horde.local.php pointing to horde dir
+        // Type horde-application needs a config/horde.local.php pointing to horde dir
+        if ($package->getType() == 'horde-application')
+        {
+            $hordeLocalFilePath = $package->getInstallDir() . '/config/horde.local.php';
+            $hordeLocalFileContent = sprintf("<?php if (!defined('HORDE_BASE')) define('HORDE_BASE'', '%s');",
+                dirname($package->getInstallDir(), 2) . '/horde/horde/' );
+            // special case horde/horde needs to require the composer autoloader
+            if ($package->getName() == 'horde/horde') {
+                $hordeLocalFileContent .= 'require_once(\'$this->vendorDir/autoload.php\');';
+                // ensure a registry.local.php exists. If not, create one containing only fileroot
+                $registryLocalFilePath = $package->getInstallDir() . '/config/registry.local.php';
+                if (!file_exists($registryLocalFilePath))
+                {
+                    $registryLocalFileContent = sprintf(
+                        "<?php\n\$app_fileroot = '%s';\n// \$app_webroot = \$this->detectWebroot()\n",
+                        $package->getInstallDir()
+                    );
+                    file_put_contents($registryLocalFilePath, $registryLocalFileContent);
+                }
+            }
+            file_put_contents($hordeLocalFilePath, $hordeLocalFileContent);
+        }
         // If a horde-application has a registry snippet in doc-dir, fetch it and put it into config/registry.d
-        // special case horde/horde needs to require the composer autoloader
         // horde-library needs to check for js/ to copy or link
-        // if horde/horde exists, ensure a registry.local.php exists. If not, create one containing only fileroot
     }
 
     /**
