2c80154a2e4dbd79530556d66845e29f0719856b
Kieran Brahney <kieran@supportpal.com>
2020-10-28 20:36:27 +0000

    composer-plugin-api 2.0 support

--- src/HordeInstallerPlugin.php
+++ src/HordeInstallerPlugin.php
@@ -13,4 +13,15 @@ class HordeInstallerPlugin implements PluginInterface
         $installer = new HordeInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        $installer = new HordeInstaller($io, $composer);
+        $composer->getInstallationManager()->removeInstaller($installer);
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        return;
+    }
 }
