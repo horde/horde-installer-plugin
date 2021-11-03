21127f61da2438f20b3dda25d2a2432f8a8a50a7
Kieran <kieran@supportpal.com>
2021-11-03 12:36:20 +0000

    composer-plugin-api 2.0 support (#4)
    * composer-plugin-api 2.0 support
    
    * Update composer.json
    
    * Add handling for LibraryInstaller returning PromiseInterface
    
    Co-authored-by: Ralf Lang <lang@b1-systems.de>

--- src/HordeInstallerPlugin.php
+++ src/HordeInstallerPlugin.php
@@ -64,4 +64,15 @@ class HordeInstallerPlugin implements PluginInterface, EventSubscriberInterface
             }
         }
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        $installer = new HordeInstaller($io, $composer);
+        $composer->getInstallationManager()->removeInstaller($installer);
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        return;
+    }
 }
