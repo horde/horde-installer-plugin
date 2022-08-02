1781c84dff773d35dfccec06df2a718bc23967d4
Ralf Lang <ralf.lang@ralf-lang.de>
2022-08-02 19:00:36 +0200

    Abstract away the different output interfaces of Command and Installer

---
+++ src/IOAdapter/ComposerIoAdapter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Common interface implemented by shims for 
+ * Composer\IO\IoInterface and Symfony\Component\Console\Output\OutputInterface
+ * Allows factoring out implementation into HordeReconfigureFlow
+ * without losing output capabilities.
+ *
+ * @internal No promise of stable interface, implementation detail 
+ */
+declare(strict_types=1);
+
+namespace Horde\Composer\IOAdapter;
+use Composer\IO\IoInterface;
+
+class SymphonyOutputAdapter implements FlowIoInterface
+{
+    private IoInterface $io;
+
+    public function __construct(IoInterface $io)
+    {
+        $this->io = $io;
+    }
+
+    public function writeln(string $content): void
+    {
+        $this->io->write($content, true);
+    }
+}
